f6b8cfbff612a92de56709e8d2dbb89e028fb2ab
Yannick <20306025+yandeu@users.noreply.github.com>
2019-01-27 13:08:23 +0100

    Changed tiles to static physics body

--- src/components/tiles/tilesSingle.ts
+++ src/components/tiles/tilesSingle.ts
@@ -1,13 +1,11 @@
 export default class TilesSingle extends Phaser.Physics.Arcade.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
     super(scene, x, y, texture)
-    scene.add.existing(this)
-    scene.physics.add.existing(this)
 
-    this.setImmovable(true)
     this.setOrigin(0, 0)
-    // @ts-ignore
-    this.body.setAllowGravity(false)
+
+    scene.add.existing(this)
+    scene.physics.add.existing(this, true)
 
     // this allows the player to jump through a tile from below
     this.body.checkCollision.down = false
