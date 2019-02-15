5ba0bbdcf48f91550aa83a4d2b199b25c4312bed
Yannick <20306025+yandeu@users.noreply.github.com>
2019-02-15 17:08:00 +0100

    Added timeout to show custom loading screen

--- src/game.ts
+++ src/game.ts
@@ -4,36 +4,35 @@ import PreloadScene from './scenes/preloadScene'
 // @ts-ignore
 import SpineWebGLPlugin from './plugins/SpineWebGLPlugin'
 
-const ratio = Math.max(window.innerWidth / window.innerHeight, window.innerHeight / window.innerWidth)
-const DEFAULT_HEIGHT = 720
-const DEFAULT_WIDTH = ratio * DEFAULT_HEIGHT //1280
-
-const config: GameConfig = {
-  type: Phaser.WEBGL,
-  backgroundColor: '#ffffff',
-  parent: 'phaser-game',
-  scale: {
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: DEFAULT_WIDTH,
-    height: DEFAULT_HEIGHT
-  },
-  plugins: {
-    scene: [{ key: 'SpineWebGLPlugin', plugin: SpineWebGLPlugin, start: true, sceneKey: 'spine' }]
-  },
-  scene: [PreloadScene, MainScene],
-  physics: {
-    default: 'arcade',
-    arcade: {
-      debug: false,
-      gravity: { y: 2500 }
-    }
-  }
-}
-
 window.addEventListener('load', () => {
-  let loadingScreen = document.getElementById('loading-screen')
-  if (loadingScreen) loadingScreen.remove()
+  window.setTimeout(() => {
+    const ratio = Math.max(window.innerWidth / window.innerHeight, window.innerHeight / window.innerWidth)
+    const DEFAULT_HEIGHT = 720
+    const DEFAULT_WIDTH = ratio * DEFAULT_HEIGHT //1280
 
-  new Phaser.Game(config)
+    const config: GameConfig = {
+      type: Phaser.WEBGL,
+      backgroundColor: '#ffffff',
+      parent: 'phaser-game',
+      scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT
+      },
+      plugins: {
+        scene: [{ key: 'SpineWebGLPlugin', plugin: SpineWebGLPlugin, start: true, sceneKey: 'spine' }]
+      },
+      scene: [PreloadScene, MainScene],
+      physics: {
+        default: 'arcade',
+        arcade: {
+          debug: false,
+          gravity: { y: 2500 }
+        }
+      }
+    }
+
+    new Phaser.Game(config)
+  }, 2000)
 })
--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -33,6 +33,9 @@ export default class MainScene extends Phaser.Scene {
   }
 
   create() {
+    let loadingScreen = document.getElementById('loading-screen')
+    if (loadingScreen) loadingScreen.remove()
+
     const map = new Map(this.level)
 
     this.cameras.main.setBackgroundColor('#ade6ff')
