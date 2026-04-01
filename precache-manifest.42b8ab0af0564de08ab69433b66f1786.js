b8feb4a3b7ddc0cc28f71d6261968d172b704802
yandeu <yandeu@users.noreply.github.com>
2026-04-01 19:13:43 +0000

    deploy: daf745ce9332f76b4b44f2700bdaef6563533d59

---
+++ precache-manifest.42b8ab0af0564de08ab69433b66f1786.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "622f30719d2a799060c8",
+    "url": "game.b3fcbb6541d6a1c1c4ba.js"
+  },
+  {
+    "revision": "fa9fb483337bb5b3790a",
+    "url": "vendors.d91782fe3aeeb8b871f5.js"
+  },
+  {
+    "revision": "260d8b0c38f307742e58f4068b91a904",
+    "url": "index.html"
+  },
+  {
+    "revision": "57040e5677322118f6d56a1d9e43c5c6",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "fce01705782b2b0cfd332771d6c1fabf",
+    "url": "sw.js"
+  },
+  {
+    "revision": "7eb17dd7992d994e9458c20e5e06ee7d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "01018acda369257b41cc1c66d897e1fb",
+    "url": "assets/icons/icons-512.png"
+  },
+  {
+    "revision": "82035e76949b35a0e630343e914c6706",
+    "url": "assets/icons/icons-192.png"
+  },
+  {
+    "revision": "171d49a74c8506688c68d953616de331",
+    "url": "assets/img/background.png"
+  },
+  {
+    "revision": "321451d7e72e30242d0298648c4a36b2",
+    "url": "assets/img/bee.png"
+  },
+  {
+    "revision": "a1fcca8be8b6b14075f15568e6f6a1d1",
+    "url": "assets/img/coin.png"
+  },
+  {
+    "revision": "0f80f7d1f852adfe06bb4fd850258912",
+    "url": "assets/img/controls.png"
+  },
+  {
+    "revision": "390e30cfd9d8352bc26fd0c07596c984",
+    "url": "assets/img/goal.png"
+  },
+  {
+    "revision": "dc4ae2aa19d995edecc0f81c74300c4f",
+    "url": "assets/img/player.png"
+  },
+  {
+    "revision": "2e65cd566ce501c75e67c45e77952ec9",
+    "url": "assets/img/slime.png"
+  },
+  {
+    "revision": "5242ead74cb23824ae5d0e2316c6c849",
+    "url": "assets/img/tile-left.png"
+  },
+  {
+    "revision": "671bbf5dcbdb8d922ab00bbd76746a18",
+    "url": "assets/img/tile-middle.png"
+  },
+  {
+    "revision": "26a50606e623a236dded5be4e3d02efd",
+    "url": "assets/spine/boy.atlas"
+  },
+  {
+    "revision": "99c3f634326e4e27229423f199f5a3f9",
+    "url": "assets/img/tile-single.png"
+  },
+  {
+    "revision": "a1d3f659688e62d4e9c0219c9f9885e4",
+    "url": "assets/img/tile-right.png"
+  },
+  {
+    "revision": "94452586b366f6514a918ee15281f2ec",
+    "url": "assets/spine/boy.png"
+  },
+  {
+    "revision": "6a109a27226054a64d66c93bd4959583",
+    "url": "assets/spine/boy.json"
+  }
+];
---
+++ sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.42b8ab0af0564de08ab69433b66f1786.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
